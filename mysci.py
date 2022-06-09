f0d52e6d13fc3cfd346cf970a75405d8f8ff3961
Mark Branson <mark.branson@colostate.edu>
2022-06-09 13:23:50 -0600

    Adding script file

--- mysci.py
+++ mysci.py
@@ -1,10 +1,24 @@
 # Read the data file
 filename = "data/wxobs20170821.txt"
-datafile = open(filename, 'r')
 
-print(datafile.readline())
-print(datafile.readline())
-print(datafile.readline())
-print(datafile.readline())
+#datafile = open(filename, 'r')
 
-datafile.close()
+# read just a few lines
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+
+# read the entire file
+#data = datafile.read()
+#datafile.close()
+
+# Read the data file (indent with four spaces on the 2nd line)
+#   this will automatically close the file when it's done reading
+#   BEST TO USE SPACES INSTEAD OF TABS
+with open(filename, 'r') as datafile:
+   data = datafile.read()
+
+# DEBUG
+print(data)
+print(type(data))
