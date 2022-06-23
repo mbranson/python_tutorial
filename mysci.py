db16b1bcd95cdbeeff6b25fb0b660ddfeddc4918
Mark Branson <mark.branson@colostate.edu>
2022-06-23 17:00:49 -0600

    Refactoring data parsing code

--- mysci.py
+++ mysci.py
@@ -1,31 +1,30 @@
-# Initialize my data variable (create an empty list variable)
-#data = []    # declare data to be an empty list
-#data = {}    # declare data to be a dictionary
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2}
 
-# create some empty lists within our dictionary
+# Data types for each column (only if non-string)
+types = {'tempout': float}
+
 # Initialize my data variable
-data = {'date': [],
-  'time': [],
-  'tempout': []}   # list[0] vs dict['key'] --> data['tempout']
+data = {}
+for column in columns:
+   data[column] = []
 
 # Read and parse the data file
 filename = "data/wxobs20170821.txt"
 with open(filename, 'r') as datafile:
 
- # Read the first three lines (header)
- for _ in range(3):
-    datafile.readline()
+   # Read the first three lines (header)
+   for _ in range(3):
+      datafile.readline()
 
- # Read and parse the rest of the file
- for line in datafile:
-    datum = line.split()
-    data['date'].append(datum[0])
-    data['time'].append(datum[1])
-    data['tempout'].append(float(datum[2]))
+   # Read and parse the rest of the file
+   for line in datafile:
+      split_line = line.split()
+      for column in columns:
+         i = columns[column]
+         t = types.get(column, str)
+         value = t(split_line[i])
+         data[column].append(value)
 
 # DEBUG
-print(data['tempout'][0:9])
-print(type(data['time']))    
-print(type(data['tempout'][9]))    
-
-   
+print(data['tempout'])   
